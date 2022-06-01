aafc797e381481e90c243bb59424c9f76d15533b
J Cole Morrison <me@jcolemorrison.com>
2022-05-31 19:19:39 -0700

    filter out wavelength zones for IPv6

--- data.tf
+++ data.tf
@@ -1,3 +1,9 @@
+# Note: filter out wavelength zones if they're enabled in the account being deployed to.
+# If these aren't filtered out, they'll throw an error when trying to assign an IPv6 CIDR block.
 data "aws_availability_zones" "available" {
   state = "available"
+  filter {
+    name   = "group-name"
+    values = [var.region]
+  }
 }
